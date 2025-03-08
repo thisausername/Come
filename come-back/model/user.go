fb3a503486d080e8e914a6fb8aec6c6b4b6106f3
sevetis <seve_tis@proton.me>
2025-03-08 22:08:15 +0800

    fix user model

--- come-back/model/user.go
+++ come-back/model/user.go
@@ -11,9 +11,9 @@ const (
 
 type User struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
-	Username  string    `gorm:"not null" json:"username"`
-	Email     string    `gorm:"unique;not null" json:"email"`
-	Password  string    `gorm:"not null" json:"-"`
+	Username  string    `gorm:"type:varchar(255);not null" json:"username"`
+	Email     string    `gorm:"type:varchar(255);unique;not null" json:"email"`
+	Password  string    `gorm:"type:varchar(255);not null" json:"-"`
 	Role      UserRole  `gorm:"not null;default:0" json:"role"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 }
